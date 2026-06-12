c70d8bed9ea49b98d3ba3758c1c05384164fc01e
Thanuja Somandepalli -X (tsomande - XORIANT CORPORATION at Cisco) <tsomande@cisco.com>
2026-06-12 15:07:50 +0530

    Testing automation

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Jenkins Pipeline"
+    return "Hello from Jenkins Pipeline!!!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
